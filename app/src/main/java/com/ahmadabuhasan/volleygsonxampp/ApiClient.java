24368b9e25bcd28130332261a96f8b9b19da763f
احمد ابو حسن <36786100+eby8zevin@users.noreply.github.com>
2021-01-10 00:12:53 +0700

    set IP Address Server

---
+++ app/src/main/java/com/ahmadabuhasan/volleygsonxampp/ApiClient.java
@@ -0,0 +1,14 @@
+package com.ahmadabuhasan.volleygsonxampp;
+
+/*
+ * Created by Ahmad Abu Hasan on 10/01/2021
+ */
+
+public class ApiClient {
+
+    public static final String URL_CREATE = "http://192.168.1.121/volleygsonxampp/create?";
+    public static final String URL_READ = "http://192.168.1.121/volleygsonxampp/read";
+    public static final String URL_UPDATE = "http://192.168.1.121/volleygsonxampp/update";
+    public static final String URL_DELETE = "http://192.168.1.121/volleygsonxampp/delete?id=";
+
+}
